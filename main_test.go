cb5deff08a1100d06d6cd9533c79a03f737a9237
Sarwesh Maharjan <sarweshmaharjan@lftechnology.com>
2023-07-14 01:45:20 +0545

    100% test coverage

--- main_test.go
+++ main_test.go
@@ -15,3 +15,21 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("Expected 5, but got %d", result)
 	}
 }
+
+func TestMain(t *testing.T) {
+	expectedAddResult := 5
+	expectedSubtractResult := 5
+
+	// Run the main function
+	main()
+
+	// Check the result of Add function
+	if result := Add(2, 3); result != expectedAddResult {
+		t.Errorf("Add function returned incorrect result. Expected: %d, Got: %d", expectedAddResult, result)
+	}
+
+	// Check the result of Subtract function
+	if result := Subtract(10, 5); result != expectedSubtractResult {
+		t.Errorf("Subtract function returned incorrect result. Expected: %d, Got: %d", expectedSubtractResult, result)
+	}
+}
