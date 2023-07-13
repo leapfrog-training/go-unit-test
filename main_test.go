ccf6a25a3001a73d410cbd2fb57d7042ac32b33c
Sarwesh Maharjan <sarweshmaharjan@lftechnology.com>
2023-07-14 01:18:30 +0545

    Fix error message

--- main_test.go
+++ main_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestAdd(t *testing.T) {
 	result := Add(2, 3)
 	if result != 5 {
-		t.Errorf("Expected 4, but got %d", result)
+		t.Errorf("Expected 5, but got %d", result)
 	}
 }
