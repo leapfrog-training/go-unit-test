fa99ced08a57c838f64531abca9f445bf45f41e0
Sarwesh Maharjan <sarweshmaharjan@lftechnology.com>
2023-07-14 01:17:50 +0545

    First unit test

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ func Add(a, b int) int {
 	return a + b
 }
 
+func Subtract(a, b int) int {
+	return a - b
+}
+
 func main() {
 	result := Add(2, 3)
+	sresult := Subtract(10, 5)
 	fmt.Println(result)
+	fmt.Println(sresult)
 }
--- main_test.go
+++ main_test.go
@@ -8,3 +8,10 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected 4, but got %d", result)
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	result := Subtract(10, 5)
+	if result != 5 {
+		t.Errorf("Expected 5, but got %d", result)
+	}
+}
